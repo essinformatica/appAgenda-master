1128b9fde93886407bf8fbad2fc5e34290b99a91
essinformatica <essinformatica15@gmail.com>
2022-01-24 15:07:12 -0300

    novo ajuste mensagem

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -111,11 +111,14 @@ class AgendaController extends Controller
                ->where('profissionals.id', '=', $profissional)
                ->Select('profissional')
                ->get();
+          $hora = json_decode($hora);
+          $servico = json_decode($servico);
+          $profissional = json_decode($profissional);
           $headers =  'MIME-Version: 1.0' . "\r\n";
           $headers .= 'From: REJANE<edmar@ed-info.net.br>' . "\r\n";
           $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
           $returnPath = "-f edmar@ed-info.net.br";
-          $mensagem = 'Seu agendamento foi realizado com sucesso!' . "<br> Data: " . $data . "<br> Hora: " . $hora['hora'] . "<br> Profissional: " . $profissional['profissional'] . "<br> Serviço:" . $servico['servico'];
+          $mensagem = 'Seu agendamento foi realizado com sucesso!' . "<br> Data: " . $data . "<br> Hora: " . $hora->hora . "<br> Profissional: " . $profissional->profissional . "<br> Serviço:" . $servico->servico;
           mail($email, 'Agendamento', $mensagem, $headers, $returnPath);
 
           $hora = null;
